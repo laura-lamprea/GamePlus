138fa08598b9d58ad41f70e8e6ab25e2fd59c72e
laura-lamprea <laura.lampream@gmail.com>
2022-04-29 00:27:39 -0500

    update pagination

--- api/src/controllers/Videogame.js
+++ api/src/controllers/Videogame.js
@@ -4,28 +4,58 @@ const { KEY } = process.env;
 const { Videogame, Genre } = require("../db");
 
 
-// 3328  The Witcher 3: Wild Hunt
+
 
 const apiGames = async (req, res) => {
-    try {
-        const datosApi = await axios.get(`https://api.rawg.io/api/games?key=${KEY}&page_size=100`); //100 y paginar 15  6.6667
-        const gamesApi = datosApi.data.results.map(g => {
-            return {
-                id: g.id,
-                name: g.name,
-                image: g.background_image,
-                rating: g.rating,
-                genres: g.genres.map(g => g.name),
-            };
-        });
-        //console.log(gamesApi);
-        //return res.status(200).send(gamesApi);
-        return gamesApi;
-    } catch (error) {
-        // console.error(error);
-        return error;
-        //return res.status(404).send('Not found');
+  try {
+
+    let totalGames = [];
+    let page = 1;
+    while (page < 6) {
+      const gamesFromPage = await axios(`https://api.rawg.io/api/games?key=${KEY}&page=${page}`);
+      let { results } = gamesFromPage.data;
+      totalGames = totalGames.concat(results);
+      page++;
     }
+
+    const gamesApi = totalGames.map(g => {
+      return {
+        id: g.id,
+        name: g.name,
+        image: g.background_image,
+        rating: g.rating,
+        genres: g.genres.map(g => g.name),
+      };
+    });
+
+    //console.log(gamesApi);
+    return (gamesApi)
+    // const URL = `https://api.rawg.io/api/games?key=${KEY}`;
+    // const arrURL = [URL, URL + '&page=2', URL + '&page=3', URL + '&page=4', URL + '&page=5'] //20 por cada uno para un total de 100
+
+    //console.log(arrURL)
+    // const datosApi = await Promise.all(arrURL.map(axios.get)); //&page_size=40 //100 y paginar 15  6.6667
+    // //console.log('DATOS API',datosApi)
+    // const gamesApi = datosApi.map(todo => todo.data.results.map(g => {
+    //   return {
+    //     id: g.id,
+    //     name: g.name,
+    //     image: g.background_image,
+    //     rating: g.rating,
+    //     genres: g.genres.map(g => g.name),
+    //   };
+    // }));
+
+
+    // const allGamesApi = gamesApi.map(pos=>pos.concat(pos))
+    //console.log(allGamesApi.length);
+    //return res.status(200).send(gamesApi);
+    //return gamesApi;
+  } catch (error) {
+    // console.error(error);
+    return error;
+    //return res.status(404).send('Not found');
+  }
 };
 
 const dbGames = async () => {
@@ -43,17 +73,18 @@ const dbGames = async () => {
 
 const getAllGames = async (req, res) => {
   const { page, name } = req.query;
+
   const dataDb = await dbGames();
-  //console.log(dataDb)
-  const dataApi = await apiGames();
-  const allGames =  [...dataDb, ...dataApi]; 
+ const dataApi = await apiGames();
+  //console.log('TAMANO', dataApi.length) /100
+  const allGames = [...dataDb, ...dataApi];
   if (name) {
     //console.log(name.toLowerCase())
     const gameByName = await allGames.filter(n => n.name.toLowerCase().includes(name.toLowerCase())); //filter porque find solo uno //== name.toLowerCase());
-    if(!gameByName.length){
-        return res.status(400).send('Not found')
-    }else{
-        return res.json(gameByName)
+    if (!gameByName.length) {
+      return res.status(400).send('Not found')
+    } else {
+      return res.json(gameByName)
     }
   }
   if (page) {
@@ -65,49 +96,49 @@ const getAllGames = async (req, res) => {
       return res.json(pages);
     }
   }
-   //return res.status(200).send(dataApi);
+  //return res.status(200).send(dataApi);
 };
 
 
 const getById = async (req, res) => {
-    const { id } = req.params;
-    //console.log(id.length) //36
-    try {
-      if(id.length != 36){
-        const game = await axios.get(`https://api.rawg.io/api/games/${id}?key=${KEY}`);
-        const info = {
-            name: game.data.name,
-            image: game.data.background_image_additional, 
-            genres: game.data.genres.map(g => g.name),
-            description: game.data.description_raw,
-            rating: game.data.rating,
-            released: game.data.released,
-        }
-        return res.status(200).send(info);
-      }if(id.length == 36){
-        const dataDb = await dbGames();
-        const gameId = await dataDb.find(g => g.id == id);
-        return res.status(200).send(gameId);
+  const { id } = req.params;
+  //console.log(id.length) //36
+  try {
+    if (id.length != 36) {
+      const game = await axios.get(`https://api.rawg.io/api/games/${id}?key=${KEY}`);
+      const info = {
+        name: game.data.name,
+        image: game.data.background_image_additional,
+        genres: game.data.genres.map(g => g.name),
+        description: game.data.description_raw,
+        rating: game.data.rating,
+        released: game.data.released,
       }
-       
-    } catch (error) {
-        return error;
-        //return res.status(404).send('Not found');
+      return res.status(200).send(info);
+    } if (id.length == 36) {
+      const dataDb = await dbGames();
+      const gameId = await dataDb.find(g => g.id == id);
+      return res.status(200).send(gameId);
     }
+
+  } catch (error) {
+    return error;
+    //return res.status(404).send('Not found');
+  }
 };
 
 const createGame = async (req, res) => {
-  const {name, image, description, released, rating, platforms, genres} = req.body;
-  const newGame = await Videogame.create({name, image, description, released, rating, platforms});
+  const { name, image, description, released, rating, platforms, genres } = req.body;
+  const newGame = await Videogame.create({ name, image, description, released, rating, platforms });
 
   // console.log('newGame', newGame)
-  const genderDb = await Genre.findAll({where: {name: genres}});
+  const genderDb = await Genre.findAll({ where: { name: genres } });
 
   console.log('genderDb', genderDb)
 
   newGame.addGenre(genderDb);
 
-  res.json({data: newGame, msg:'Successful create'});
+  res.json({ data: newGame, msg: 'Successful create' });
 };
 
 const deleteGame = async (req, res) => {
@@ -127,7 +158,7 @@ const deleteGame = async (req, res) => {
 };
 
 module.exports = {
-    getAllGames,
-    getById,
-    createGame
+  getAllGames,
+  getById,
+  createGame
 };
