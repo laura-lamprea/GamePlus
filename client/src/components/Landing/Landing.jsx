cfcf21e203173b268255ce6dcb195df97edd37e1
laura-lamprea <laura.lampream@gmail.com>
2022-05-04 13:23:49 -0500

    LandingPage css

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -6,11 +6,18 @@ import L from './Landing.module.css';
 export default function LadingPage() {
     return (
         <div className={L.container}>
-            {/* <img className={L.imgL} height='150px' src={logo} alt="logo" />  
-            <img  className={L.img2} height='300px' src={pokes} alt="logo" />   */}
-            <Link to='/home'>
-            <button className={L.btnGo}>GO!</button>
-            </Link>
+            <div className={L.containerInfo}>
+                <h2>WELCOME TO GAME+</h2>
+                <h1>ONLINE GAMING SITE</h1>
+                <p>Here you can see the different
+                    video games available along with relevant information
+                    about them using the rawg external api.</p>
+                <Link to='/home'>
+                    <button className={L.btnGo}>Let's go!</button>
+                </Link>
+            </div>
+
+
         </div>
     )
 };
