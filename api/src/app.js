0b30e6f7ea7a5404fdd99ce521bf3a76baf0962f
laura-lamprea <laura.lampream@gmail.com>
2022-05-10 21:03:46 -0500

    release deploy

--- api/src/app.js
+++ api/src/app.js
@@ -10,13 +10,16 @@ const server = express();
 
 server.name = 'API';
 
+//agregado heroku 
+server.use(cors())
+
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  // res.header('Access-Control-Allow-Origin', '*'); //* para habilitar todos los endpoints
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); //* para habilitar todos los endpoints
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
