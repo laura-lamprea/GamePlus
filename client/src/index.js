51d1fd558b11bdc211eef4738ba71dccfedf3f51
laura-lamprea <laura.lampream@gmail.com>
2022-05-19 20:16:52 -0500

    Changes to vercel

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+import axios from 'axios';
 import dotenv from "dotenv";
 dotenv.config();
 
